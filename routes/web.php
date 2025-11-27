52875610c2ad3f23f98180b58d48e8165d295e78
Victor Marques <w4rqu3s@gmail.com>
2025-11-27 19:03:18 -0300

    Definição provisória das rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BebidasController;
+use App\Http\Controllers\TiposController;
+use App\Http\Controllers\UsersController;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/bebidas', BebidasController::class);
+Route::resource('/tipos', TiposController::class);
+Route::resource('/usuarios', UsersController::class);
