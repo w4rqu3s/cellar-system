902a18ab572a81aefb4382d550adfac295249e9e
Victor Marques <w4rqu3s@gmail.com>
2025-11-28 01:58:16 -0300

    Estilização

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,48 +1,36 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4 position-relative">
     <div class="container-fluid">
 
-        <a class="navbar-brand" href="{{ url('/') }}">
-            Adega App
-        </a>
+        {{-- Bloco esquerdo: Home --}}
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ url('/') }}">Home</a>
+            </li>
+        </ul>
 
-        <button 
-            class="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNav"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarNav">
-
-            <ul class="navbar-nav">
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('bebidas.index', 'adega') }}">
-                        Sua Adega
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('bebidas.index', 'desejos') }}">
-                        Lista de Desejos
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('tipos.index') }}">
-                        Tipos
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('usuarios.index') }}">
-                        Usuários
-                    </a>
-                </li>
-
-            </ul>
+        {{-- Switch centralizado --}}
+        <div class="position-absolute start-50 translate-middle-x">
+            <div class="btn-group" role="group" aria-label="Lista Toggle">
+                <a href="{{ route('bebidas.index', 'adega') }}"
+                   class="btn {{ request()->route('lista') == 'adega' ? 'btn-light text-primary' : 'btn-outline-light' }}">
+                    Sua Adega
+                </a>
+                <a href="{{ route('bebidas.index', 'desejos') }}"
+                   class="btn {{ request()->route('lista') == 'desejos' ? 'btn-light text-primary' : 'btn-outline-light' }}">
+                    Lista de Desejos
+                </a>
+            </div>
         </div>
+
+        {{-- Bloco direito: outros links --}}
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('tipos.index') }}">Tipos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('usuarios.index') }}">Usuários</a>
+            </li>
+        </ul>
+
     </div>
 </nav>
